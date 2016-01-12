d31f490c5918dc7d790112528cc5e0636063d0b1
cloudcome <cloudcome@163.com>
2016-01-13 00:29:38 +0800

    + unit test

---
+++ test/index.js
@@ -0,0 +1,23 @@
+/**
+ * 文件描述
+ * @author ydr.me
+ * @create 2016-01-12 23:35
+ */
+
+
+'use strict';
+
+var assert = require('assert');
+
+describe('coolie-html-attr-resource', function () {
+    it('sorry', function () {
+        console.error('-----------------------------------');
+        console.error('-----------------------------------');
+        console.error('sorry');
+        console.error('can not run unit test now');
+        console.error('coolie api will update');
+        console.error('-----------------------------------');
+        console.error('-----------------------------------');
+    })
+});
+
